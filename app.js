f356b604604e5ac9e445805365734049bcaa4709
James Sinker <j@Jamess-MacBook-Pro.local>
2019-11-04 09:58:58 -0800

    adds function to make deck of cards 2-10 of each suit

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
-let pElement = document.querySelector('p');
-let htmlElement = document.querySelector('html');
+//let pElement = document.querySelector('p');
+//let htmlElement = document.querySelector('html');
 
 //function to toggle hidden class
-let stylechange = function (e) {
-	let target = getEventTarget(e);
-	target.classList.toggle('hidden');
-}
+//let stylechange = function (e) {
+//	let target = getEventTarget(e);
+//	target.classList.toggle('hidden');
+//}
 
 
 //this gets the specific element that was clicked..
-let getEventTarget = function (event) {
-	console.log(event.target);
-	return event.target;
-}
+//let getEventTarget = function (event) {
+//	console.log(event.target);
+//	return event.target;
+//}
 
 //By adding to html it will work on all elements on page..
-htmlElement.addEventListener('click', stylechange);
+//htmlElement.addEventListener('click', stylechange);
+
+//make the deck 52 cards, 13 of each type, 4 suits.
+let makeDeck = function() {
+	let deck = [];
+	let suits = ['clubs', 'hearts', 'spades', 'diamonds'];
+	for (suit of suits) {
+		for (let i = 2; i <= 10; i++) {
+		let newcard = {};
+		newcard.suit = suit;
+		newcard.value = i;
+		deck.push(newcard);
+		}
+	}
+	return deck;
+}
+
+let deck = makeDeck();
+console.log(deck);
+
+
 
