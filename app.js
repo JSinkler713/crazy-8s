3746b49468f3327ebed991b4e77d8d58f4fed9a1
James Sinker <j@Jamess-MacBook-Pro.local>
2019-11-04 16:25:12 -0800

    fixed logic by using strict mode and getting rid of card global variable

--- app.js
+++ app.js
@@ -1,107 +1,101 @@
-//let pElement = document.querySelector('p');
-let htmlElement = document.querySelector('html');
-
-//function to toggle hidden class
-let allChanges = function (e) {
-	//find out which element was targeted
-	let target = getEventTarget(e);
-	if(target.classList[0] == "deck") {
-		//draw a card and put in player hand
-		draw();
-	}
-}
-
-
-//this gets the specific element that was clicked..
-let getEventTarget = function (event) {
-	console.log(event.target);
-	return event.target;
-}
-
-//By adding to html it will work on all elements on page..
-htmlElement.addEventListener('click', allChanges);
+//will stop you with global variables
+'use strict'
 
 //make the deck 52 cards, 13 of each type, 4 suits.
 let makeDeck = function() {
 	let deck = [];
 	let suits = ['clubs', 'hearts', 'spades', 'diamonds'];
-	for (suit of suits) {
+	for (let suit of suits) {
 		for (let i = 2; i <= 10; i++) {
-		let newcard = {};
-		newcard.suit = suit;
-		newcard.value = i;
-		deck.push(newcard);
+			let newcard = {};
+			newcard.suit = suit;
+			newcard.value = i;
+			deck.push(newcard);
 		}
 	}
+	console.log("Make Deck: ", deck)
 	return deck;
 }
 
 let deck = makeDeck();
-console.log(deck);
 //shuffle deck
 //Need to complete
 
 
-//deal the player 7 cards
-let deal = function(deck) {
-  //assume deck was shuffled for now
-  let playerHand = deck.splice(0, 7);
-  return playerHand;
+//deal the player 7 cards need to connect to DOM
+let dealSeven = function(deck) {
+	//assume deck was shuffled for now
+	let playerHand = deck.splice(0, 7);
+	console.log("Deal Seven: ", playerHand)
+	return playerHand;
 }
-let playerHand = deal(deck);
-let updateHand = function () {
-        for (card of playerHand) {
-                let text = card.value + " of " + card.suit + "'s";
-                console.log(text);
-                let cardDiv = document.createElement('div');
-                cardDiv.innerText = text;
-                cardDiv.classList.add('card');
-                document.querySelector(".player-hand").append(cardDiv);
-        }
-}
-console.log(playerHand);
-updateHand();
-//initialize playedCards array
-let playedCards = [];
+let playerHand = dealSeven(deck);
 
-//flip over the starting card
-let initCard = function() {
-  let firstCardPlayed = deck.splice(0, 1);
-  playedCards.push(firstCardPlayed[0]);
+let updateHand = function (playerHand) {
+	console.log("Update Hand")
+	for (let card of playerHand) {
+		console.log("Card: ", card)
+		let cardDiv = document.createElement('div');
+		cardDiv.innerText = `${card.value} of ${card.suit}`
+		cardDiv.classList.add('card');
+		cardDiv.addEventListener('click', function() {
+			playCard(card);
+		});
+		document.querySelector(".player-hand").append(cardDiv);
+	}
 }
+updateHand(playerHand)
 
-console.log(deck);
+let playFirstCard = function(deck, playedCards) {
+	playedCards.push(deck.pop())
+}
+let playedCards = []
+playFirstCard(deck, playedCards)
+
 
 let playedCardFunc = function () {
   //this should push a card into the array and then display the top card
-  //the top card is the one thats just been added.
-  
-  for (card of playedCards) {
-        let text = card.value + " of " + card.suit + "'s";
-        console.log(text);
+    for (card of playedCards) {
         let cardDiv = document.createElement('div');
-        cardDiv.innerText = text;
-        cardDiv.classList.add('card');
+        cardDiv.innerText = `${card.value} of ${card.suit}`;
+		cardDiv.classList.add('card');
         document.querySelector(".played-cards").append(cardDiv);
 	}
 }
+//function to allow player to play a card into the "Played" area
+let playCard = function (card) {
+	//see which card was clicked-its the div that represents it - not the object
+	//chec if it has the same "suit" or "value" or is an "eight"
+	console.log("User playing: ", card);
+	console.log("Compare to: ", playedCards[0])
+	//console.log(playedCards[0].value);
+	//console.log(playedCards[0].suit);
+	if(card.suit == playedCards[0].suit || card.value == playedCards[0].value) {
+		console.log("LEGIT!")
+	}
+	else {
+		console.log("BOO")
+	}
+}
+
+
 
 //function to allow player to draw from the deck
 let draw = function () {
   let newCard = deck.splice(0,1)[0];
-  console.log(newCard);
   playerHand.push(newCard);
-  let text = newCard.value + " of " + newCard.suit + "'s";
-  console.log(text);
   let cardDiv = document.createElement('div');
-  cardDiv.innerText = text;
+  cardDiv.innerText = `${newCard.value} of ${newCard.suit}`
   cardDiv.classList.add('card');
+  cardDiv.addEventListener('click', function() {
+	  playCard(newCard);
+  });
   document.querySelector(".player-hand").append(cardDiv);
 }
   
 
+let deckElement = document.querySelector('.deck');
+deckElement.addEventListener('click', draw);
 
-
-initCard();
-playedCardFunc();
-console.log(deck);
+//playedCardFunc();
+//console.log(deck);	
